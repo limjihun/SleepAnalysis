a66b9de8a73de18cf9e74c04a7f3c52534ac1959
SeonWooKim <ukus04@gmail.com>
2018-10-30 00:48:26 +0900

    switching_activity

--- app/src/main/java/com/sleepanalysis/mobilecomputing/MeasureActivity.java
+++ app/src/main/java/com/sleepanalysis/mobilecomputing/MeasureActivity.java
@@ -1,7 +1,12 @@
 package com.sleepanalysis.mobilecomputing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MeasureActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MeasureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measure);
+
+        // button_result 클릭 시 ResultActivity로 전환
+        ImageButton button_result = findViewById(R.id.result_button);
+        button_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MeasureActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        long start_time, end_time, sleep_time;
+
+        // start 클릭 시 시간 측정 시작, end로 문구 변경
+        final Button button_start = findViewById(R.id.start_button);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button_start.setText(getText(R.string.end));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/sleepanalysis/mobilecomputing/ResultActivity.java
+++ app/src/main/java/com/sleepanalysis/mobilecomputing/ResultActivity.java
@@ -1,7 +1,10 @@
 package com.sleepanalysis.mobilecomputing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        // button_measure 클릭 시 MeasureActivity로 전환
+        ImageButton button_measure = findViewById(R.id.measure_button);
+        button_measure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultActivity.this, MeasureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
