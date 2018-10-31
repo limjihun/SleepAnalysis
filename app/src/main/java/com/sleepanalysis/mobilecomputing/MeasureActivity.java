defd3f89ddfd3408dfb8c4f348e7b3da1b43bed9
SeonWooKim <ukus04@gmail.com>
2018-11-01 00:50:01 +0900

    measure light in measureActivity(Just showing real-time brightness)

--- app/src/main/java/com/sleepanalysis/mobilecomputing/MeasureActivity.java
+++ app/src/main/java/com/sleepanalysis/mobilecomputing/MeasureActivity.java
@@ -1,6 +1,11 @@
 package com.sleepanalysis.mobilecomputing;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,13 +14,17 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MeasureActivity extends AppCompatActivity {
+public class MeasureActivity extends AppCompatActivity implements SensorEventListener {
 
     final static int START = 0;
     final static int END = 1;
 
     int status = START;
     long start_time, end_time, sleep_time = 0L;
+
+    SensorManager mSensorManager;
+    Sensor mLightSensor;
+    TextView light_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,10 @@ public class MeasureActivity extends AppCompatActivity {
             }
         });
 
+        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        light_info = findViewById(R.id.light_test);
+
         // start 클릭 시 시간 측정 시작, end로 문구 변경
         final Button button_start = findViewById(R.id.start_button);
         button_start.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +58,9 @@ public class MeasureActivity extends AppCompatActivity {
                     case START :
                         button_start.setText(getText(R.string.end));
                         start_time = System.currentTimeMillis();
+
+                        mSensorManager.registerListener(MeasureActivity.this, mLightSensor, SensorManager.SENSOR_DELAY_UI);
+
                         status = END;
                         Log.d("Start time: ", String.valueOf(start_time));
                         break;
@@ -52,6 +68,9 @@ public class MeasureActivity extends AppCompatActivity {
                     case END :
                         button_start.setText(getText(R.string.start));
                         end_time = System.currentTimeMillis();
+
+                        mSensorManager.unregisterListener(MeasureActivity.this);
+
                         status = START;
                         Log.d("End time: ", String.valueOf(end_time));
                         break;
@@ -60,7 +79,14 @@ public class MeasureActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    }
 
+    public void onSensorChanged(SensorEvent event){
+        if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+            light_info.setText(String.valueOf(event.values[0]));
+        }
     }
 }
