32a3c0c5b54839e50b8c785c42efefbcfdbb205e
limjihun <lim794@naver.com>
2018-10-31 15:16:42 +0900

    시간초 측정

--- app/src/main/java/com/sleepanalysis/mobilecomputing/ResultActivity.java
+++ app/src/main/java/com/sleepanalysis/mobilecomputing/ResultActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -12,6 +13,19 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        TextView sleep_time_result = findViewById(R.id.sleep_time_result);
+
+        Intent intent = getIntent();
+
+        // sleep_time 받아와서 띄우기
+        int sleep_time = (int)intent.getExtras().getLong("sleep_time");
+        int h, m, s;
+        h = sleep_time / (1000 * 60 * 60);
+        m = (sleep_time / (1000 * 60)) % 60;
+        s = (sleep_time / 1000) % 60;
+        sleep_time_result.setText( // Sleep time : h:mm:ss
+                String.format(getResources().getString(R.string.sleep_time_result), h, m, s));
 
         // button_measure 클릭 시 MeasureActivity로 전환
         ImageButton button_measure = findViewById(R.id.measure_button);
