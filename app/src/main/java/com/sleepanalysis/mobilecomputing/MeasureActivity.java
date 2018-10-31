45dd711ee65127d540ae85e529cc421432f9dac2
limjihun <lim794@naver.com>
2018-10-31 21:20:27 +0900

    누락사항추가

--- app/src/main/java/com/sleepanalysis/mobilecomputing/MeasureActivity.java
+++ app/src/main/java/com/sleepanalysis/mobilecomputing/MeasureActivity.java
@@ -3,12 +3,19 @@ package com.sleepanalysis.mobilecomputing;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MeasureActivity extends AppCompatActivity {
+
+    final static int START = 0;
+    final static int END = 1;
+
+    int status = START;
+    long start_time, end_time, sleep_time = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +28,36 @@ public class MeasureActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MeasureActivity.this, ResultActivity.class);
+
+                sleep_time = end_time - start_time;
+                intent.putExtra("sleep_time", sleep_time);
+                Log.d("Sleep time : ", String.valueOf(sleep_time));
                 startActivity(intent);
             }
         });
-
-        long start_time, end_time, sleep_time;
 
         // start 클릭 시 시간 측정 시작, end로 문구 변경
         final Button button_start = findViewById(R.id.start_button);
         button_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                button_start.setText(getText(R.string.end));
+                switch (status) {
+                    case START :
+                        button_start.setText(getText(R.string.end));
+                        start_time = System.currentTimeMillis();
+                        status = END;
+                        Log.d("Start time: ", String.valueOf(start_time));
+                        break;
+
+                    case END :
+                        button_start.setText(getText(R.string.start));
+                        end_time = System.currentTimeMillis();
+                        status = START;
+                        Log.d("End time: ", String.valueOf(end_time));
+                        break;
+
+                }
+
             }
         });
 
